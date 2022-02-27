2966a9256dd54b61978e6861454671c0993c3496
pilna-tylos <egle.naslenaite@gmail.com>
2022-02-27 21:17:34 +0200

    No skeleton upload

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <main class="container">
 
     <div class="side-wrapper">
-      <p>Raise your arms!</p>
+      <p title="Make sure your arms are visible to your camera">Raise your arms!</p>
       <p><span :class="{'hidden': !leftHandUp}">✋</span><span :class="{'hidden': !rightHandUp}">🤚</span></p>
       <p :class="{'hidden': !bothArmsUp}" class="success-msg">Well Done!</p>
       <audio ref="wellDoneAudio" src="./assets/sound/Well_Done_sound.mp3" type="audio/mpeg" />
     </div>
 
     <div class="video-wrapper">
-      <canvas ref="canvas" />
+<!--      <canvas ref="canvas" />-->
       <video ref="video" :srcObject.prop="videoStream" muted autoplay playsinline />
       <p v-if="accessDenied" class="no-camera-msg">Unable to access your camera.</p>
     </div>
@@ -19,10 +19,10 @@
 </template>
 
 <script>
-// import {Pose } from "@mediapipe/pose";
-import {Pose, POSE_CONNECTIONS } from "@mediapipe/pose";
+import {Pose } from "@mediapipe/pose";
+// import {Pose, POSE_CONNECTIONS } from "@mediapipe/pose";
 import {Camera} from "@mediapipe/camera_utils";
-import { drawConnectors, drawLandmarks} from "@mediapipe/drawing_utils"
+// import { drawConnectors, drawLandmarks} from "@mediapipe/drawing_utils"
 
 export default {
   name: 'App',
@@ -35,32 +35,28 @@ export default {
       videoStream: false,
       accessDenied: false,
 
-      canvasElement: '',
-      canvasCtx: ""
+      // canvasElement: '',
+      // canvasCtx: ""
     }
   },
   mounted() {
+    this.accessCamera();
     this.enableMpPose();
   },
   methods: {
-    // accessCamera() {
-    //   navigator.mediaDevices.getUserMedia({video:true}).then( stream => {
-    //     this.videoStream = stream;
-    //     console.log("got stream");
-    //     console.log("got stream");
-    //     console.log(stream);
-    //     console.log(stream.getVideoTracks());
-    //
-    //   }).catch(error => {
-    //     console.error(error);
-    //     this.accessDenied = true;
-    //   })
-    // },
+    accessCamera() {
+      navigator.mediaDevices.getUserMedia({video:true}).then( stream => {
+        this.videoStream = stream;
+      }).catch(error => {
+        console.error(error);
+        this.accessDenied = true;
+      })
+    },
 
     enableMpPose(){
       const videoElement = this.$refs.video;
-      this.canvasElement = this.$refs.canvas;
-      this.canvasCtx = this.canvasElement.getContext('2d');
+      // this.canvasElement = this.$refs.canvas;
+      // this.canvasCtx = this.canvasElement.getContext('2d');
 
       const pose = new Pose({locateFile: (file) => {
           return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
@@ -89,12 +85,12 @@ export default {
       if (!results.poseLandmarks) return;
 
       // Draw skeleton
-      this.canvasCtx.save();
-      this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-
-      drawConnectors(this.canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {color: '#ffffff', lineWidth: 2});
-      drawLandmarks(this.canvasCtx, results.poseLandmarks, {color: '#ffffff', fillColor: '#131d47',lineWidth: 2, radius: 3});
-      this.canvasCtx.restore();
+      // this.canvasCtx.save();
+      // this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+      //
+      // drawConnectors(this.canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {color: '#ffffff', lineWidth: 2});
+      // drawLandmarks(this.canvasCtx, results.poseLandmarks, {color: '#ffffff', fillColor: '#131d47',lineWidth: 2, radius: 3});
+      // this.canvasCtx.restore();
 
       this.leftArmRisen(results);
       this.rightArmRisen(results);
@@ -156,11 +152,11 @@ export default {
     display: block;
   }
 
-  canvas {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-  }
+  /*canvas {*/
+  /*  position: absolute;*/
+  /*  width: 100%;*/
+  /*  height: 100%;*/
+  /*}*/
 
   .no-camera-msg {
     margin: 0;
